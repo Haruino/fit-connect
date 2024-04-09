350e3cda2e8ac457495ac521afec957e074938fd
Haruino <harukiinoue5270@gmail.com>
2024-04-09 14:54:18 +0000

    [Add]routing追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'homes/top'
-  # 顧客用
+  # 会員用
   # URL /user/sign_in ...
   devise_for :user,skip: [:passwords], controllers: {
     registrations: "public/registrations",
@@ -12,9 +12,22 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-
+  
+  root to: "homes#top"
+  
+  # ユーザー用
   scope module: :public do
-    resource :records, except: [:edit]
+    resource :records, except: [:new,:index,:edit]
+    resources :past_records, except: [:new,:create,:edit]
+    resources :groups, except: [:new,:destroy] do
+      resources :group_users, only: [:create,:destroy], param: :user_id
+      resources :post_threads, except: [:new]
+    end
+  end
+  
+  # 管理者用
+  namespace :admin do
+    
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
