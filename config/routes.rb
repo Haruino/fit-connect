e16f7f3af8a1f37f542e2b051b6540d3ef99927b
Haruino <harukiinoue5270@gmail.com>
2024-04-10 07:00:45 +0000

    [Fix]Routing修正

--- config/routes.rb
+++ config/routes.rb
@@ -17,17 +17,37 @@ Rails.application.routes.draw do
   
   # ユーザー用
   scope module: :public do
-    resource :records, except: [:new,:index,:edit]
-    resources :past_records, except: [:new,:create,:edit]
-    resources :groups, except: [:new,:destroy] do
-      resources :group_users, only: [:create,:destroy], param: :user_id
-      resources :post_threads, except: [:new]
+    resource  :records,         except: [:new,:index,:edit]
+    resources :past_records,    except: [:new,:create,:edit]
+    resources :users,           only:   [:show,:update,:edit] do
+      get :favorites,             on: :member
+      get :withdraw,              on: :member
+      resource :relationships,    only: [:create,:destroy] 
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers'  => 'relationships#followers',  as: 'followers'
+    end
+    resources :groups,          except: [:new,:destroy] do
+      resources :group_users,   only:   [:create,:destroy], param: :user_id
+      resources :post_threads,  except: [:new,:index,:create], as: 'post_threads', shallow: true do
+        resources :comments,      only: [:create]
+        delete 'comments/:id',    to: 'comments#destroy', as: 'comment'
+        resources :favorites,     only:   [:create]
+        delete 'favorites/:id',   to: 'favorites#destroy', as: 'favorite'
+      end
     end
   end
   
   # 管理者用
   namespace :admin do
-    
+    get 'comments'
+    resources :users,           except: [:new,:create,:destroy] 
+    resources :genres,          except: [:new,:show]
+    resources :groups,          only:   [:index,:show,:destroy] do
+      resources :group_users,     only:   [:destroy]
+      resources :post_threads,    except: [:new,:create], shallow: true do
+        delete 'comments/:id',      to: 'comments#destroy', as: 'comment'
+      end
+    end
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
