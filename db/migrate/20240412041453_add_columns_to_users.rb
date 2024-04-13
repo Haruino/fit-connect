5e5e07d3242466536ddd24b1e69348606961bf9d
Haruino <harukiinoue5270@gmail.com>
2024-04-13 05:37:33 +0000

    [Fix]マイグレーションファイル変更

--- db/migrate/20240412041453_add_columns_to_users.rb
+++ db/migrate/20240412041453_add_columns_to_users.rb
@@ -1,7 +1,7 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :name, :string, null: false
-    add_column :users, :introduction, :text, null: false, default: ""
+    add_column :users, :introduction, :text, null: true
     add_column :users, :active, :boolean, null: false, default: true
   end
 end
