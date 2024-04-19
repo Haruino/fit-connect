1dd476f7ea89efe5becaae03266e21cc31b6a700
Haruino <harukiinoue5270@gmail.com>
2024-04-19 07:50:50 +0000

    [Add]Admin機能

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -2,15 +2,18 @@ class Admin::CommentsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @comments = Comment.all
+    @comments = Comment.includes(:post_thread).all
   end
   
   def destroy
-    @post_thread = PostThread.find(params[:id])
     @comment = Comment.find(params[:id])
-    @post_thread = @comment.post_thread
-    if @comment.destroy
+    @post_thread = @comment.post_thread 
+    if params[:from_page] == "post_thread_show"
+      @comment.destroy
       redirect_to admin_post_thread_path(@post_thread)
+    elsif params[:from_page] == "comments_index"
+      @comment.destroy
+      redirect_to admin_comments_path
     else
       render "admin/post_threads/show"
     end
