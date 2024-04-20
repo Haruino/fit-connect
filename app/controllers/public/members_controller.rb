967ba2939ebab5e67e01c109553278bd469c9895
Haruino <harukiinoue5270@gmail.com>
2024-04-20 08:53:39 +0000

    [Fix]Membersコントローラ

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -4,20 +4,20 @@ class Public::MembersController < ApplicationController
 
   def index
   end
-  
+
   def create
     current_user.members.create(group: @group)
     redirect_to request.referer
   end
-  
+
   def destroy
-    member = current_user.members.find_by(group: @group)
+    member = Member.find(params[:id])
     member.destroy
     redirect_to request.referer
   end
-  
+
   private
-  
+
   def set_group
     @group = Group.find(params[:group_id])
   end
