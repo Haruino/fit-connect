9831e8a078a175f72126d0d1d2fe3afc93c9536f
Haruino <harukiinoue5270@gmail.com>
2024-04-21 11:28:32 +0000

    [Add]フォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :members,          dependent: :destroy
-  has_many :groups,           through: :members
+  has_many :groups,           through:  :members
   has_many :post_threads,     dependent: :destroy
   has_many :comments,         dependent: :destroy
   has_many :favorites,        dependent: :destroy
