e7f471b975fb282678f5fe0f31bbb30f5a926452
Haruino <harukiinoue5270@gmail.com>
2024-04-28 11:15:35 +0000

    [Fix]修正

--- app/controllers/public/records_controller.rb
+++ app/controllers/public/records_controller.rb
@@ -15,10 +15,14 @@ class Public::RecordsController < ApplicationController
       render :show
     end
   end
-  
+
   def show
     todays_records_select
     @record = Record.new
+    if current_user.guest_user?
+      flash[:alert] = "ゲストユーザーは記録機能を使用できません。"
+      redirect_to root_path
+    end
   end
 
   def update
@@ -38,7 +42,7 @@ class Public::RecordsController < ApplicationController
       render :show
     end
   end
-  
+
   def parts_exercises
     @user = User.find(params[:user_id])
     @parts = @user.parts
@@ -46,23 +50,23 @@ class Public::RecordsController < ApplicationController
   end
 
   private
-  
+
   def set_user
     @user = User.find(params[:user_id])
   end
-  
+
   def find_record
     @record = @user.records.find(params[:id])
   end
-  
+
   def latest_set_number(exercise_id, part_id)
     latest_record = @user.records.where(part_id: part_id, exercise_id: exercise_id).where("DATE(created_at) = ?", Date.today).order(created_at: :desc).first
     latest_record ? latest_record.set : 0
   end
-  
+
   def todays_records_select
     records = @user.records.select(:id, :name, :rep, :set, :weight, :memo, :created_at).group_by(&:name)
-    @todays_records = records.select { |name, records| 
+    @todays_records = records.select { |name, records|
       records.any? { |record| record.created_at.to_date == Date.today }
     }.transform_values { |records| records.select { |record| record.created_at.to_date == Date.today } }
   end
