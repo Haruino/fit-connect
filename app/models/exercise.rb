5e5b79dbb3b42ad75452e49828aff82f37ce5b5a
Haruino <harukiinoue5270@gmail.com>
2024-05-03 10:21:33 +0000

    [Fix]修正

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,5 +2,5 @@ class Exercise < ApplicationRecord
   belongs_to  :user
   has_many    :records, dependent: :destroy
   
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { maximum: 20 }, uniqueness: { scope: :user_id, case_sensitive: false }
 end
